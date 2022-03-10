f355a15a19989f2f52bb2c0b1dc17aeb9c789573
128_Tanmay Trivedi_PG-DAC-KH <100686400+TanmayTrivedi@users.noreply.github.com>
2022-03-11 00:55:13 +0530

    Delete Assignment 2 directory

--- Assignment 2/Assignment_2_1.java
+++
@@ -1,8 +0,0 @@
-class Assignment_2_1
-{
-	public static void main(String args[])
-		{
-		 System.out.println("Hello");
-		 System.out.print("Alexandra Abramov");
-		}
-}
--- Assignment 2/Assignment_2_3.java
+++
@@ -1,9 +0,0 @@
-class Assignment_2_3
-{
-	public static void main(String args[])
-	{
-		int x = 50;
-		int y = 3 ;
-		System.out.println("Division is "+(x/y));
-	}
-}
